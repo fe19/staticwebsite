390c15eae03c6b9bb99afa91c4cfb365438c646d
fe19 <flth741@gmail.com>
2024-09-01 15:23:35 +0200

    add cat image

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import catLogo from './assets/cat.jpg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -19,15 +20,10 @@ function App() {
       <h1>Hello Helen, how are you doing?</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Click on me
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+              <img src={catLogo} className="logo cat" alt="Cat logo" />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
